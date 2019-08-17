c4dd51b20cf681c80b8906e35c7ad8037fd7e3a9
Chad Edwards <chad@fergusapp.com>
2019-08-17 21:49:26 +1200

    Actually fix issue

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000 || process.env.PORT, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
